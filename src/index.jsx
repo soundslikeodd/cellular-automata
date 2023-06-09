f9cfb21a64baa7f3d6410b01b148ad374a4f5bdc
soundlikeodd <mattschaadt@gmail.com>
2023-06-08 20:55:18 -0700

    feat: added package info to web console

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,20 @@
 import { createRoot } from 'react-dom/client';
 import CellularAutomata from './CellularAutomata';
+import { webPackageBanner } from '@soundslikeodd/package-banner';
+import standard from 'figlet/importable-fonts/Standard';
+import packageJson from '../package.json';
 
 const root = createRoot(document.getElementById('cellular-automata-app'));
 root.render(<CellularAutomata />);
+
+webPackageBanner({
+    packageJson,
+    capitalCase: true,
+    breakOnWord: true,
+    figletFontFileData: standard,
+    additionalPackageInfo: [
+        'description',
+        'author',
+        'license',
+    ],
+});
